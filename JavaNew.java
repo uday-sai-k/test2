cf23338e2ec736f1331ae89f5c57f173b6d6da12
uday-sai-k <saisumauday@gmail.com>
2023-09-20 09:18:41 +0530

    test2 commit tested

---
+++ JavaNew.java
@@ -0,0 +1,13 @@
+class JavaNew 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("I am into test repo");
+		System.out.println("I am into test repo");
+		System.out.println("I am into test repo");
+		System.out.println("I am into test repo");
+		System.out.println("I am into test repo");
+		System.out.println("I am into test repo");
+		System.out.println("from the test 2");
+	}
+}
